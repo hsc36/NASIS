d9aa27f036f8528b56e55831baab5552fce04ef1
hsc36 <hsc36@drexel.edu>
2014-11-05 17:08:55 -0500

    Update of Arduino Codes
    Launcher Code: v1.14.11.5 - Ready for testing
    Rocket Code: v0.14.11.5 - Need hardware setup completed before code is
    completed

--- Launcher and Rocket Codes/launcher/launcher.ino
+++ Launcher and Rocket Codes/launcher/launcher.ino
@@ -1,30 +1,30 @@
-//  Name: Hillel Chaitoff
-//  Date: OCT 15 2014
-//  Note: Launcher Code
-
-// Imports
-#include <dht11.h>
+// Name: Hillel Chaitoff
+// Date: OCT 15 2014
+// Note: Launcher Code
 
 // Definitions
-dht11 DHT;
-#define DHT11_PIN 6  // Humidity & Temperature PIN
 #define LAUNCH_RELAY 5  // Relay PIN
 
 // Variables
 char ID[] = "000001";
-boolean powerOn = false;
-char poweredOn[] = "{\"" + ID + "\":{\"poweredOn\":\"True\"}}";
-char received[] = "";
+boolean poweredOn = false;
+char powerOn[] = "{\"" + ID + "_launcher" + "\":{\"powerOn\":\"True\"}}"; // Launcher Powered On
+char received[] = ""; // Message Received Over Serial (from RaspberryPi)
 boolean initialized = false;
-char initCommand[] = "";
+char initializeOn[] = "{\"" + ID + "_launcher" + "\":{\"initialized\":\"True\"}}"; // Launcher Initialized (Prepping for Launch)
+char initCommand[] = "{\"" + ID + "_launcher" + "\":{\"initCommand\":\"True\"}}"; // RaspberryPi Sent Initialization Command
+boolean confirmIR = false;
+boolean confirmSound = false;
+boolean confirmFlame = false;
+boolean launchConfirmed = false;
 boolean launched = false;
-char launchCommand[] = "";
-
+char launchConfirmOn[] = "{\"" + ID + "_launcher" + "\":{\"launched\":\"True\"}}"; // Rocket Launch Confirmed
+char launchConfirmOff[] = "{\"" + ID + "_launcher" + "\":{\"launched\":\"False\"}}"; // Rocket Launch Not Confirmed
+char launchCommand[] = "{\"" + ID + "_launcher" + "\":{\"launchCommand\":\"True\"}}"; // RaspberryPi Sent Launch Command
 
 int IR_PIN = 3;  // IR Distance Sensor on A3
 int SOUND_PIN = 4;  // Sound Sensor on A4
 int FLAME_PIN = 5;  // Flame Sensor on A5
-int sensorValue = 0;
 
 // Setup Loop
 void setup(){
@@ -32,45 +32,101 @@ void setup(){
   // Rocket Launch Signal (Relay PIN)
   pinMode(LAUNCH_RELAY, OUTPUT);  // Mode
   digitalWrite(LAUNCH_RELAY, LOW)  // Initial Value
-  
 }
 
 // Process Loop
 void loop(){
-  // If First Power-On:
-  if(powerOn == false){
-    //  Send "PoweredOn" over Serial
-    Serial.write(poweredOn);
-    powerOn = true;
+
+  // If Power-On:
+  if(!poweredOn){
+    // Send "PoweredOn" over Serial
+    Serial.write(powerOn);
+    poweredOn = true;
   }
-  
-  // Read from the Serial for a Command
-  received = Serial.read()
-  // If "InitializeCommand" is received:
-  if(received == initCommand){
-    //  Go from STANDBY to FULL_POWER mode
-    initialized = true;
-    //  Send Confirmations of Initialization over Serial
-  }
-  
-  // If "LaunchCommand" is received and "isInitialized":
-  if((received == launchCommand) && initialized){
-    //  Launch Rocket
-    digitalWrite(LAUNCH_RELAY, HIGH);
-    //  Confirm Launch by Reading the IR Data
-    // If Confirmed:
-    if(launchConfirmed){
-      launched = true;
-    }else{
-      //  Report Launch Failure
+  if(!initialized){
+    // Read from the Serial for a Command
+    received = Serial.read()
+    // If "InitializeCommand" is received:
+    if(received == initCommand){
+      // Go from STANDBY to FULL_POWER mode
+      initialized = true;
+      // Send Confirmations of Initialization over Serial
+      Serial.write(initializeOn);
     }
   }
-  
-  // If Launched:
-  if(launched){
-    //  Send Data over Serial
-    digitalWrite(LAUNCH_RELAY, LOW);
+  if(!launched){
+    // If "LaunchCommand" is received and "isInitialized":
+    if((received == launchCommand) && initialized){
+      // Launch Rocket
+      digitalWrite(LAUNCH_RELAY, HIGH);
+      // @NOTE: Options  1. WAIT, then get data from all 3 sensors before reporting successful launch
+      //                *2. WHILE NO confirmation for each of 3 keep checking for confirmation
+      // Confirm Launch by Reading the IR, Sound, and Flame Data
+      int sensorCount = 0;
+      while(!(confirmFlame && confirmSound && confirmIR) || (sensorCount > 299)){ // 30 Seconds
+        // 1: Check for Flame
+        if((!confirmFlame)){
+          confirmFlame = checkFlame();
+          delay(33.333333333);  // Delay One-Thirtieth of a Second
+        }
+        // 2: Check for Sound
+        if((!confirmSound)){
+          confirmSound = checkSound();
+          delay(33.333333333);  // Delay One-Thirtieth of a Second
+        }
+        // 3: Check IR (if Rocket Present)
+        if((!confirmIR)){
+          confirmIR = checkIR();
+          delay(33.333333333);  // Delay One-Thirtieth of a Second
+        }
+        // If Confirmed:
+        if(confirmFlame && confirmSound && confirmIR){
+          // Launch Confirmation
+          launchConfirmed = true;
+        }else{
+          // No Launch Confirmation
+          launchConfirmed = false;
+        }
+        sensorCount = sensorCount + 1;
+      }
+      // Launch Action Performed
+      launched = true;
+    }
+    // If Launched:
+    if(launched){
+      // Switch off the Relay
+      digitalWrite(LAUNCH_RELAY, LOW);
+      // Send Data over Serial
+      if(launchConfirmed){
+        Serial.write(launchConfirmOn);
+      }else{
+        Serial.write(launchConfirmOff);
+      }
+    }
   }
-  
 }
 
+// Check if Rocket Launch Made Expected Flame
+boolean checkFlame(){
+  if(analogRead(FLAME_PIN) < 201){  // Resolution = 0-1023
+    return true;
+  }else{
+    return false;
+  }
+}
+// Check if Rocket Launch Made Expected Sound
+boolean checkSound(){
+  if(analogRead(SOUND_PIN) > 509){  // Resolution = 0-1023
+    return true;
+  }else{
+    return false;
+  }
+}
+// Check if Rocket Left the Launcher
+boolean checkIR(){
+  if(((analogRead(IRpin) * 0.0048828125) > 5.0){ // Distance (cm) = IR_PIN * (5/1024)
+    return true;
+  }else{
+    return false;
+  }
+}
--- Launcher and Rocket Codes/rocket/rocket.ino
+++ Launcher and Rocket Codes/rocket/rocket.ino
@@ -1,31 +1,66 @@
-//  Name: Hillel Chaitoff
-//  Date: OCT 15 2014
-//  Note: Rocket Code
+// Name: Hillel Chaitoff
+// Date: OCT 15 2014
+// Note: Rocket Code
 
 // Imports
 
-// Pins Variables
+// Variables
+char ID[] = "000001";
+boolean poweredOn = false;
+char powerOn[] = "{\"" + ID + "_rocket" + "\":{\"powerOn\":\"True\"}}"; // Launcher Powered On
+char received[] = ""; // Message Received Over Serial (from RaspberryPi)
+boolean initialized = false;
+char initializeOn[] = "{\"" + ID + "_rocket" + "\":{\"initialized\":\"True\"}}"; // Launcher Initialized (Prepping for Launch)
+char initCommand[] = "{\"" + ID + "_rocket" + "\":{\"initCommand\":\"True\"}}"; // RaspberryPi Sent Initialization Command
+boolean launched = false;
+char launchCommand[] = "{\"" + ID + "_rocket" + "\":{\"launchCommand\":\"True\"}}"; // RaspberryPi Sent Launch Command
+char dataStr[] = "";	// Sensor Data
 
-// Data Packaging Variables
+// @TODO: Sensor PINs
 
 // Setup Loop
 void setup(){
-  
+  Serial.begin(9600); //Set Serial BaudeRate
 }
 
 // Process Loop
 void loop(){
-  // If First Power-On:
-  //  Send "PoweredOn" over Serial
-  
-  // Read from the Serial for a Command
-  
-  // If "InitializeCommand" is received:
-  //  Go from STANDBY to FULL_POWER mode
-  //  Send Confirmations of Initialization over Serial
-  
+  // If Power-On:
+  if(!poweredOn){
+    // Send "PoweredOn" over Serial
+    Serial.write(powerOn);
+    poweredOn = true;
+  }
+  if(!initialized){
+    // Read from the Serial for a Command
+    received = Serial.read()
+    // If "InitializeCommand" is received:
+    if(received == initCommand){
+      // Go from STANDBY to FULL_POWER mode
+      initialized = true;
+      // Send Confirmations of Initialization over Serial
+      Serial.write(initializeOn);
+    }
+  }
   // If "LaunchCommand" is received and "isInitialized":
-  //  Start Sendin
-  //  Send Confirmation of Successful Launch over Serial
-  
+  if(!launched){
+  	// Check if Launch Command Received
+  	if(received == launchCommand){
+  		launched = true;
+  	}
+  }
+  if(launched){
+  	// Get Sensor Data
+  	dataStr = collectData();
+  	// Start Sending
+  	Serial.write("{\"" + ID + "_rocket" + "\":{\"data\":" + dataStr + "}}");
+  }
+}
+
+// Collect and Package Data from Sensors
+char collectData(){
+	// Get Data from Sensors
+	// @TODO: Need Sensor Setup To Obtain Data
+	// Compile into JSON-Formatted String
+	// @TODO: Data to Strings, then Format
 }
