b241fee439e4500ccd1e4c568924249a36e4decb
hsc36 <hsc36@drexel.edu>
2014-11-13 01:44:16 -0500

    Technical Changes to Launcher Code
    Switched from char arrays to Strings for simplicity of code.
    It allows for easier comparisons of information that is posted to the
    serial.
    NOTE: It is understood that the terminator character (i.e. '\n' in this
    case) is NOT included in the String that is returned from
    "Serial.readStringUntil('\n')".

--- Launcher and Rocket Codes/launcher/launcher.ino
+++ Launcher and Rocket Codes/launcher/launcher.ino
@@ -6,21 +6,21 @@
 #define LAUNCH_RELAY 5  // Relay PIN
 
 // Variables
-char ID[] = "000001";
+String ID = "000001";
 boolean poweredOn = false;
-char powerOn[] = "{\"" + ID + "_launcher" + "\":{\"powerOn\":\"True\"}}"; // Launcher Powered On
-char received[] = ""; // Message Received Over Serial (from RaspberryPi)
+String powerOn = "{\"000001_launcher\":{\"powerOn\":\"True\"}}"; // Launcher Powered On
+String received = ""; // Message Received Over Serial (from RaspberryPi)
 boolean initialized = false;
-char initializeOn[] = "{\"" + ID + "_launcher" + "\":{\"initialized\":\"True\"}}"; // Launcher Initialized (Prepping for Launch)
-char initCommand[] = "{\"" + ID + "_launcher" + "\":{\"initCommand\":\"True\"}}"; // RaspberryPi Sent Initialization Command
+String initializeOn = "{\"000001_launcher\":{\"initialized\":\"True\"}}"; // Launcher Initialized (Prepping for Launch)
+String initCommand = "{\"000001_launcher\":{\"initCommand\":\"True\"}}"; // RaspberryPi Sent Initialization Command
 boolean confirmIR = false;
 boolean confirmSound = false;
 boolean confirmFlame = false;
 boolean launchConfirmed = false;
 boolean launched = false;
-char launchConfirmOn[] = "{\"" + ID + "_launcher" + "\":{\"launched\":\"True\"}}"; // Rocket Launch Confirmed
-char launchConfirmOff[] = "{\"" + ID + "_launcher" + "\":{\"launched\":\"False\"}}"; // Rocket Launch Not Confirmed
-char launchCommand[] = "{\"" + ID + "_launcher" + "\":{\"launchCommand\":\"True\"}}"; // RaspberryPi Sent Launch Command
+String launchConfirmOn = "{\"000001_launcher\":{\"launched\":\"True\"}}"; // Rocket Launch Confirmed
+String launchConfirmOff = "{\"000001_launcher\":{\"launched\":\"False\"}}"; // Rocket Launch Not Confirmed
+String launchCommand = "{\"000001_launcher\":{\"launchCommand\":\"True\"}}"; // RaspberryPi Sent Launch Command
 
 int IR_PIN = 3;  // IR Distance Sensor on A3
 int SOUND_PIN = 4;  // Sound Sensor on A4
@@ -31,7 +31,7 @@ void setup(){
   Serial.begin(9600); //Set Serial BaudeRate
   // Rocket Launch Signal (Relay PIN)
   pinMode(LAUNCH_RELAY, OUTPUT);  // Mode
-  digitalWrite(LAUNCH_RELAY, LOW)  // Initial Value
+  digitalWrite(LAUNCH_RELAY, LOW);  // Initial Value
 }
 
 // Process Loop
@@ -40,21 +40,23 @@ void loop(){
   // If Power-On:
   if(!poweredOn){
     // Send "PoweredOn" over Serial
-    Serial.write(powerOn);
+    Serial.print(powerOn);
     poweredOn = true;
   }
   if(!initialized){
     // Read from the Serial for a Command
-    received = Serial.read()
+    received = Serial.readStringUntil('\n');
     // If "InitializeCommand" is received:
     if(received == initCommand){
       // Go from STANDBY to FULL_POWER mode
       initialized = true;
       // Send Confirmations of Initialization over Serial
-      Serial.write(initializeOn);
+      Serial.print(initializeOn);
     }
   }
   if(!launched){
+    // Read from the Serial for a Command
+    received = Serial.readStringUntil('\n');
     // If "LaunchCommand" is received and "isInitialized":
     if((received == launchCommand) && initialized){
       // Launch Rocket
@@ -63,21 +65,21 @@ void loop(){
       //                *2. WHILE NO confirmation for each of 3 keep checking for confirmation
       // Confirm Launch by Reading the IR, Sound, and Flame Data
       int sensorCount = 0;
-      while(!(confirmFlame && confirmSound && confirmIR) || (sensorCount > 299)){ // 30 Seconds
+      while(!(confirmFlame && confirmSound && confirmIR) || (sensorCount > 299)){ // 3 Seconds
         // 1: Check for Flame
         if((!confirmFlame)){
           confirmFlame = checkFlame();
-          delay(33.333333333);  // Delay One-Thirtieth of a Second
+          delay(3.3333333333);  // Delay One-Thirtieth of a Second
         }
         // 2: Check for Sound
         if((!confirmSound)){
           confirmSound = checkSound();
-          delay(33.333333333);  // Delay One-Thirtieth of a Second
+          delay(3.3333333333);  // Delay One-Thirtieth of a Second
         }
         // 3: Check IR (if Rocket Present)
         if((!confirmIR)){
           confirmIR = checkIR();
-          delay(33.333333333);  // Delay One-Thirtieth of a Second
+          delay(3.3333333333);  // Delay One-Thirtieth of a Second
         }
         // If Confirmed:
         if(confirmFlame && confirmSound && confirmIR){
@@ -98,9 +100,9 @@ void loop(){
       digitalWrite(LAUNCH_RELAY, LOW);
       // Send Data over Serial
       if(launchConfirmed){
-        Serial.write(launchConfirmOn);
+        Serial.print(launchConfirmOn);
       }else{
-        Serial.write(launchConfirmOff);
+        Serial.print(launchConfirmOff);
       }
     }
   }
@@ -114,6 +116,7 @@ boolean checkFlame(){
     return false;
   }
 }
+
 // Check if Rocket Launch Made Expected Sound
 boolean checkSound(){
   if(analogRead(SOUND_PIN) > 509){  // Resolution = 0-1023
@@ -122,9 +125,10 @@ boolean checkSound(){
     return false;
   }
 }
+
 // Check if Rocket Left the Launcher
 boolean checkIR(){
-  if(((analogRead(IRpin) * 0.0048828125) > 5.0){ // Distance (cm) = IR_PIN * (5/1024)
+  if((analogRead(IR_PIN) * 0.0048828125) > 5.0){ // Distance (cm) = IR_PIN * (5/1024)
     return true;
   }else{
     return false;
